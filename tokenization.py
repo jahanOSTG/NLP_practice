60223f9212f52c36bc5b7a8864cd5e03a7ccde25
Faria Jahan Janie <janiefaria315@gmail.com>
2025-10-03 22:09:58 +0600

    Add files via upload

---
+++ tokenization.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+text= "I am Faria Jahan Janie.I don't love coding"
+sentence=nltk.sent_tokenize(text)
+words=nltk.word_tokenize(text)
+
+print("Sentence Tokenization :",sentence)
+print("Word Tokenization",words)
